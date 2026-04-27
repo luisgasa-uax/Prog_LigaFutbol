e96c2ba4ac5ab497db76313f38c36c0e1bb611a4
V Villalba Martínez <vvillalbamartinez1501@gmail.com>
2026-04-27 12:21:46 +0200

    feat: atributos, getters, setters y toString de jugador

--- src/LigaFutbol/Modelo/Jugador.java
+++ src/LigaFutbol/Modelo/Jugador.java
@@ -2,7 +2,38 @@ package LigaFutbol.Modelo;
 
 import LigaFutbol.Modelo.Enumeraciones.TipoPosicion;
 
-public class Jugador {
+public class Jugador extends Fichaje {
+    private int dorsal;
+    private TipoPosicion posicion;
 
-    private TipoPosicion posicion = TipoPosicion.
+    public Jugador(int id,String nombre, int precio, int dorsal, TipoPosicion posicion) {
+        super(id,nombre,precio);
+        this.dorsal = dorsal;
+        this.posicion = posicion;
+    }
+
+
+    public int getDorsal() {
+        return dorsal;
+    }
+
+    public void setDorsal(int dorsal) {
+        this.dorsal = dorsal;
+    }
+
+    public TipoPosicion getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(TipoPosicion posicion) {
+        this.posicion = posicion;
+    }
+
+    @Override
+    public String toString(){
+        String resultado;
+        resultado = String.format("%s - [%d] Posición; %s",super.toString(),dorsal,posicion);
+        return resultado;
+    }
+
 }
