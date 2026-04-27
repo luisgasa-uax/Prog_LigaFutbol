12d31cc79b78846f490bae91abb39550d311a8c9
V Villalba Martínez <vvillalbamartinez1501@gmail.com>
2026-04-27 12:16:57 +0200

    feat: Equipo atributo arraylist fichajes, metodos para buscar fichajes, dar de alta, dar de baja y listar fichajes

--- src/LigaFutbol/Controlador/Equipo.java
+++ src/LigaFutbol/Controlador/Equipo.java
@@ -1,4 +1,95 @@
 package LigaFutbol.Controlador;
 
+
+import LigaFutbol.Modelo.Fichaje;
+
+import java.util.ArrayList;
+
 public class Equipo {
+    private ArrayList<Fichaje> fichajes;
+
+    public Equipo() {
+        this.fichajes = new ArrayList();
+    }
+
+    public String listarFichajes(){
+        StringBuilder resultado = new StringBuilder();
+        for (Fichaje fichaje : fichajes){
+            resultado.append(fichaje.toString());
+        }
+        return resultado.toString();
+    }
+
+    // alta fichaje
+    public boolean darFichajeDeAlta(Fichaje fichaje){
+        boolean resultado;
+        if (buscarFichajePorId(fichaje.getId()) != null){
+            resultado = true;
+        } else {
+            resultado = false;
+        }
+        return resultado;
+    }
+
+    // baja fichaje por id
+    public boolean darFichajeDeBajaPorId(int id){
+        boolean resultado;
+        Fichaje fichajeAQuitar;
+
+        fichajeAQuitar = buscarFichajePorId(id);
+        if (fichajeAQuitar != null){
+            resultado = true;
+            fichajes.remove(fichajeAQuitar);
+        }
+        else {
+            resultado = false;
+        }
+        return resultado;
+    }
+
+    // baja fichaje por nombre
+    public boolean darFichajeDeBajaPorNombre(String nombre){
+        boolean resultado;
+        Fichaje fichajeAQuitar;
+
+        fichajeAQuitar = buscarFichajePorNombre(nombre);
+        if (fichajeAQuitar != null){
+            resultado = true;
+            fichajes.remove(fichajeAQuitar);
+        }
+        else {
+            resultado = false;
+        }
+        return resultado;
+    }
+
+    // modificar fichaje por id
+    // modificar fichaje por nombre
+
+
+    // buscar fichaje por id
+    public Fichaje buscarFichajePorId(int id){
+        Fichaje resultado = null;
+
+        for (Fichaje fichaje: fichajes){
+            if (fichaje != null && fichaje.getId() == id){
+                resultado =  fichaje;
+            }
+        }
+        return resultado;
+    }
+
+    // buscar fichaje por nombre
+    public Fichaje buscarFichajePorNombre(String nombre){
+        Fichaje resultado = null;
+
+        for (Fichaje fichaje: fichajes){
+            if (fichaje.getNombre() != null && fichaje.getNombre().equals(nombre)){
+                resultado =  fichaje;
+            }
+        }
+        return resultado;
+    }
+
+
 }
