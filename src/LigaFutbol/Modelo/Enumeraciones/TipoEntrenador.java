d62023e69092d26c6a93b049914d781d277aebcd
Alejandro Ernesto <aerneher@myuax.com>
2026-04-27 11:05:11 +0200

    Add TipoEntrenador enum with values and methods

--- src/LigaFutbol/Modelo/Enumeraciones/TipoEntrenador.java
+++ src/LigaFutbol/Modelo/Enumeraciones/TipoEntrenador.java
@@ -1,5 +1,23 @@
 package LigaFutbol.Modelo.Enumeraciones;
 
 public enum TipoEntrenador {
-// primer o segundo
+
+    primerEntrenador(1, "Mister"),
+    segundoEntrenador(2, "Segundo mister");
+
+    private int valor;
+    private String descripcion;
+
+    TipoEntrenador(int valor, String descripcion) {
+        this.valor = valor;
+        this.descripcion = descripcion;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
 }
