7c47e747fece108561ef211992fc3e7cea1306d7
V Villalba Martínez <vvillalbamartinez1501@gmail.com>
2026-04-27 11:14:10 +0200

    feat: atributos básicos de fichaje y to String

--- src/LigaFutbol/Modelo/Fichaje.java
+++ src/LigaFutbol/Modelo/Fichaje.java
@@ -1,4 +1,22 @@
 package LigaFutbol.Modelo;
 
 public class Fichaje {
+    private String nombre;
+    private int precio;
+
+    public Fichaje(String nombre, int precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    @Override
+    public String toString(){
+        String resultado;
+        resultado = String.format("%s",nombre);
+        return resultado;
+    }
 }
